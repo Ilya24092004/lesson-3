bddeaa68c40edd4fc9103e34d2f1cf5854b5d1e7
Ilya24092004 <ilya.tapilin@mail.ru>
2024-08-31 09:31:14 +0300

    Add files via upload

---
+++ lesson 3.py
@@ -0,0 +1,5 @@
+homework = 12
+hours = 1.5
+name_of_course ='Python.'
+TimeForOneQuestion = hours / homework
+print("Курс:", name_of_course, "Всего задач:",homework,"Затрачено часов:",hours,"Среднее время выполнения:", TimeForOneQuestion,"часа")
